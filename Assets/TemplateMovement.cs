f2d6bec6d08e7093a999645fa1f46e6e6a200d72
rinakaura <32963573+rinakaura@users.noreply.github.com>
2020-01-16 09:29:47 -0800

    Add files via upload

---
+++ Assets/TemplateMovement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TemplateMovement : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject finalObject;
+
+    private Vector2 mousePos;
+
+    [SerializeField]
+    private LayerMask allTilesLayer;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 horizontal = new Vector3(Mathf.Round(Input.GetAxis("Horizontal")), 0.0f, 0.0f);
+        transform.position = transform.position + horizontal *Time.deltaTime;
+        //mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //transform.position = new Vector2(Mathf.Round(mousePos.x), Mathf.Round(mousePos.y));
+    }
+}
