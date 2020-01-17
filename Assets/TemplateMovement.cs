df62862c4b42487c00e8f0c4f6db35c3f6f88c49
rinakaura <32963573+rinakaura@users.noreply.github.com>
2020-01-17 09:24:28 -0800

    Add files via upload

--- Assets/TemplateMovement.cs
+++ Assets/TemplateMovement.cs
@@ -17,7 +17,9 @@ public class TemplateMovement : MonoBehaviour
     void Update()
     {
         Vector3 horizontal = new Vector3(Mathf.Round(Input.GetAxis("Horizontal")), 0.0f, 0.0f);
-        transform.position = transform.position + horizontal *Time.deltaTime;
+        transform.position = transform.position + horizontal * Time.deltaTime*50;
+        Vector3 vertical = new Vector3(0.0f, Mathf.Round(Input.GetAxis("Vertical")), 0.0f);
+        transform.position = transform.position + vertical * Time.deltaTime*50;
         //mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         //transform.position = new Vector2(Mathf.Round(mousePos.x), Mathf.Round(mousePos.y));
     }
